1bdf6f7874c225023501e0a07dcf26ecfec49037
Lachlan Adetre <toomenvy@gmail.com>
2015-04-21 10:53:34 +1000

    Files Update
    asd

---
+++ MainController.js
@@ -0,0 +1,8 @@
+app.controller('MainController', ['$scope', function($scope) {
+    $scope.title = 'Dynifer Designs';
+    $scope.promo = "Your Own String";
+    $scope.product = {
+    	name:"The Book of Trees",
+    	price: 19
+    };
+}]);
