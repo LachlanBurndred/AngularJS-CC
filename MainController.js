07d2ed0333b98a8d20d519cf0db53a56883594a3
Lachlan Adetre <toomenvy@gmail.com>
2015-04-21 12:48:09 +1000

    Added CSS, Product IMG, Expressions for Angular

--- MainController.js
+++ MainController.js
@@ -1,8 +1,12 @@
 app.controller('MainController', ['$scope', function($scope) {
-    $scope.title = 'Dynifer Designs';
+    $scope.title = 'DyNight Designs';
     $scope.promo = "Your Own String";
     $scope.product = {
-    	name:"The Book of Trees",
-    	price: 19
+    	name:"Skulduggery Pleasant",
+    	price: 19.99
+    };
+    $scope.product_desc = {
+        name: 'Skulduggery Pleasant',
+        desc: 'A description of the book'
     };
 }]);
