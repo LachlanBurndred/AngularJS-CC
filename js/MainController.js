10e73413c214fa888ce455c9ab4ff9adeef5c739
Lachlan Adetre <toomenvy@gmail.com>
2015-04-25 17:15:06 +1000

    Added dislike button

--- js/MainController.js
+++ js/MainController.js
@@ -8,7 +8,8 @@ app.controller('MainController', ['$scope', function($scope) {
     desc: "The first book in the series of an award winning fantasy universe.",
     pubdate: new Date('2014', '03', '08'), 
     cover: 'img/skulduggery.jpg',
-    likes: 0 
+    likes: 0,
+    dislikes: 0
   }, 
   { 
     name: "Assassin's Creed: Renaissance", 
@@ -16,7 +17,8 @@ app.controller('MainController', ['$scope', function($scope) {
     desc: "A Novel by Oliver Bowden, based off the events that take place in the video game series Assassin's Creed.",
     pubdate: new Date('2007', '10', '26'), 
     cover: 'img/Renaissance.jpg',
-    likes: 0
+    likes: 0,
+    dislikes: 0
   },
   {
     name: "Harry Potter and the Philosopher's Stone",
@@ -24,11 +26,15 @@ app.controller('MainController', ['$scope', function($scope) {
     desc: "The first in a series of the most popular fantasy universe in the world.",
     pubdate: new Date('1997', '5', '26'),
     cover: 'img/harry_potter1.jpg',
-    likes: 0
+    likes: 0,
+    dislikes: 0
   }
 
 ];
+    $scope.minusOne = function(index) {
+        $scope.products[index].dislikes +=1;
+    }
     $scope.plusOne = function(index) {
         $scope.products[index].likes += 1;
     }
-}]);
+}]); 
